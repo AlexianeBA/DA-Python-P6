afb34a5de3f9c3c7b5de9e415b045e2d49ce04c4
AlexianeBA <barbealexiane05@gmail.com>
2023-11-14 16:37:45 +0100

    display all movies from categories

--- index.js
+++ index.js
@@ -49,7 +49,7 @@ function display7BestMovies(movies) {
 fetchBestMovies();
 
 function fetchActionMovies(){
-    const url = "http://localhost:8000/api/v1/titles/?year=&min_year=&max_year=&imdb_score=&imdb_score_min=&imdb_score_max=&title=&title_contains=&genre=action&genre_contains=&sort_by=&director=&director_contains=&writer=&writer_contains=&actor=&actor_contains=&country=&country_contains=&lang=&lang_contains=&company=&company_contains=&rating=&rating_contains="
+    const url = "http://localhost:8000/api/v1/titles/?&imdb_score=&imdb_score_min=&imdb_score_max=&genre=action&genre_contains=&sort_by=-imdb_score&page_size=8"
     fetch(url)
         .then(response => response.json())
         .then(data => display7BestActionMovies(data.results))
@@ -87,7 +87,7 @@ function display7BestActionMovies(movies){
 fetchActionMovies();
 
 function fetchRomanceMovie(){
-    const url = "http://localhost:8000/api/v1/titles/?actor=&actor_contains=&company=&company_contains=&country=&country_contains=&director=&director_contains=&genre=romance&genre_contains=&imdb_score=&imdb_score_max=&imdb_score_min=&lang=&lang_contains=&max_year=&min_year=&page=2&rating=&rating_contains=&sort_by=&title=&title_contains=&writer=&writer_contains=&year="
+    const url = "http://localhost:8000/api/v1/titles/?&imdb_score=&imdb_score_min=&imdb_score_max=&genre=romance&genre_contains=&sort_by=-imdb_score&page_size=8"
     fetch(url)
         .then(response => response.json())
         .then(data => display7BestRomanceMovies(data.results))
@@ -124,7 +124,7 @@ function display7BestRomanceMovies(movies){
 fetchRomanceMovie();
 
 function fetchHistoryMovie(){
-    const url = "http://localhost:8000/api/v1/titles/?actor=&actor_contains=&company=&company_contains=&country=&country_contains=&director=&director_contains=&genre=history&genre_contains=&imdb_score=&imdb_score_max=&imdb_score_min=&lang=&lang_contains=&max_year=&min_year=&page=2&rating=&rating_contains=&sort_by=&title=&title_contains=&writer=&writer_contains=&year="
+    const url = "http://localhost:8000/api/v1/titles/?&imdb_score=&imdb_score_min=&imdb_score_max=&genre=history&genre_contains=&sort_by=-imdb_score&page_size=8"
     fetch(url)
         .then(response => response.json())
         .then(data => display7BestHistoryMovies(data.results))
